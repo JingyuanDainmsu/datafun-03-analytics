03ce301d8c06ea597289a4e4c20b3915b28ef8d1
Jingyuan Dai <S571358@nwmissouri.edu>
2024-02-14 11:57:05 -0500

    add .gitignore, cmds to readme

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\daiji\Documents\DataScienceCourse\datafun-03-JingyuanDai\.venv
+set VIRTUAL_ENV=C:\Users\daiji\Documents\DataScienceCourse\datafun-03-analytics\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
